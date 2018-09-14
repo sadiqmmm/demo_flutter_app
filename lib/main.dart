7eb6381f8d6ea30b6226c1935d93514f15dffb51
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-09-15 03:38:48 +0530

    Add theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,9 @@ class EasyList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
       home: Scaffold(
           appBar: AppBar(
             title: Text("Easy List"),
