dbe129672cab1cc7469babfbff988d21fce5566c
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-09-15 03:26:54 +0530

    add initState()

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,13 @@ void main() => runApp(EasyList());
 
 class EasyList extends StatelessWidget {
   @override
- 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Easy List"),
-        ),
-        body:  ProductManager()
-      ),
+          appBar: AppBar(
+            title: Text("Easy List"),
+          ),
+          body: ProductManager('Food Tester')),
     );
   }
 }
