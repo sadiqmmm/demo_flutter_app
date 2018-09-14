7c3b3033b0e3551ff1620fd97cc1017eaf64ccc4
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-09-15 03:18:02 +0530

    Refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,18 @@
 import 'package:flutter/material.dart';
+import './product_manager.dart';
 
 void main() => runApp(EasyList());
 
-class EasyList extends StatefulWidget {
+class EasyList extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _EasyListState();
-  }
-}
-
-class _EasyListState extends State<EasyList> {
-  List<String> _products = ['Food Tester'];
-
-  @override
+ 
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Easy List"),
         ),
-        body: Column(children: [
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    _products.add('Advanced Food Tester');
-                  });
-                },
-                child: Text("Add Product")),
-          ),
-          Column(
-            children: _products
-                .map((element) => Card(
-                      child: Column(children: <Widget>[
-                        Image.asset('assets/food.jpg'),
-                        Text(element)
-                      ]),
-                    ))
-                .toList(),
-          )
-        ]),
+        body:  ProductManager()
       ),
     );
   }
